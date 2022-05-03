36bbe36991b6f6a1d8cc0e4c8ac93fc218cf0602
Grzegorz Gozdziewski <grzegorz.gozdziewski@ppuc.pl>
2022-05-03 12:54:21 +0200

    fix: change styles to scss

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [AuthenticationComponent],
+  declarations: [AuthenticationComponent, ModalComponent],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [AuthenticationComponent],
 })
