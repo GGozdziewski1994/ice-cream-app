28798abd459beba2ac5fd7c1a4ee8da3a020d45b
Grzegorz Gozdziewski <grzegorz.gozdziewski@ppuc.pl>
2022-05-08 12:06:05 +0200

    fix: remove repeat ice cream from unified list

--- src/app/iceman/order-summary/order-summary.component.ts
+++ src/app/iceman/order-summary/order-summary.component.ts
@@ -3,6 +3,7 @@ import { map, Observable } from 'rxjs';
 import { Order } from 'src/app/shared/model/order';
 import { UserOrder } from 'src/app/shared/model/userOrde';
 import { IcemanService } from 'src/app/shared/services/iceman/iceman.service';
+import { orderReducer } from 'src/app/store/order/order.reducer';
 
 @Component({
   selector: 'app-order-summary',
@@ -12,35 +13,39 @@ import { IcemanService } from 'src/app/shared/services/iceman/iceman.service';
 })
 export class OrderSummaryComponent implements OnInit {
   public orderSummary$!: Observable<Order[]>;
+  public array!: Order[];
 
   constructor(private icemnaService: IcemanService) {}
 
   ngOnInit(): void {
     this.orderSummary$ = this.icemnaService.getOrdersList().pipe(
-      map((el) =>
+      map((el) => {
+        const arr: Order[] = [];
         el.flatMap((order: UserOrder) =>
           order.order.flatMap((order) => {
-            const array: Order[] = [];
-            const existIndex = array.findIndex(
-              (el) => el.name === order.name && el.capacity === order.capacity
-            );
-            const exist = array[existIndex];
-            let updateIces: Order[] = [];
-
-            if (exist) {
-              const updateIce = {
-                ...exist,
-                amounnt: exist.amount + order.amount,
-              };
-              updateIces = [...array];
-              updateIces[existIndex] = updateIce;
-            } else {
-              updateIces = array.concat(order);
-            }
-            return updateIces;
+            arr.push(order);
           })
-        )
-      )
+        );
+        let updateIces: Order[] = [];
+        for (let order of arr) {
+          const existIndex = updateIces.findIndex(
+            (el) => el.name === order.name && el.capacity === order.capacity
+          );
+
+          const exist = updateIces[existIndex];
+
+          if (exist) {
+            const updateIce = {
+              ...exist,
+              amount: exist.amount + order.amount,
+            };
+            updateIces[existIndex] = updateIce;
+          } else {
+            updateIces = updateIces.concat(order);
+          }
+        }
+        return updateIces;
+      })
     );
   }
 }
